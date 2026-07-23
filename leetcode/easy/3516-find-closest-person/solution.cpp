a7c8e0a38738f3394a5d1ceaa234227fd5c583a9
kanchan kapri <kaprikanchan09@gmail.com>
2026-07-24 00:00:05 +0530

    ✅ Find Closest Person | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 8.6 MB | 2026-07-23

--- leetcode/easy/3516-find-closest-person/solution.cpp
+++ leetcode/easy/3516-find-closest-person/solution.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     int findClosest(int x, int y, int z) {
-        // home -- collge
-        // same position -- to_real_home
-        // distance formula 
-        // Compare the distances from Persons 1 and 2 to Person 3 to determine the answer.
-        // ghar --> l1 - 4km  
-        // same road --> 2l - 10 km
-        // p1 - p2 
+        
         int dist1 = abs(x-z);
         int dist2 = abs(y-z);
 
