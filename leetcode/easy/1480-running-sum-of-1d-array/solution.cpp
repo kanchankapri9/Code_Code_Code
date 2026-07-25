49d0040334172a60505a3c0d1ffee46dbc7bf089
kanchan kapri <kaprikanchan09@gmail.com>
2026-07-25 22:40:10 +0530

    ✅ Running Sum of 1d Array | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 12.5 MB | 2026-07-25

---
+++ leetcode/easy/1480-running-sum-of-1d-array/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+    // int sum = 0;
+    for (int i = 1; i < nums.size(); i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+};
+
+
+// 1+ 1+2+ 1+2+3
+
+// 
