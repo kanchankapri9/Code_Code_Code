e24bd928e7eb8d0dd848ced80eedc89e78098e35
kanchan kapri <kaprikanchan09@gmail.com>
2026-08-06 21:32:33 +0530

    ✅ Smallest Divisible Digit Product I | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 8.5 MB | 2026-08-06

---
+++ leetcode/easy/3345-smallest-divisible-digit-product-i/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+    //  the product of digits
+    int digitProduct(int num) {
+        int product = 1;
+
+        while (num > 0) {
+            int digit = num % 10;
+            product *= digit;
+            num /= 10;
+        }
+
+        return product;
+    }
+
+    int smallestNumber(int n, int t) {
+
+        while (true) {
+            int product = digitProduct(n);
+
+            if (product % t == 0)
+                return n;
+
+            n++;
+        }
+    }
+};
