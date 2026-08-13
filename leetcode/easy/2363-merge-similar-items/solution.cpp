8ea8c09322a0ad75a6250ac557f81e79b39a3144
kanchan kapri <kaprikanchan09@gmail.com>
2026-08-13 06:53:05 +0530

    ✅ Merge Similar Items | Easy | C++ | Runtime: 0 ms | Memory: 8.7 MB | 2026-08-12

--- leetcode/easy/2363-merge-similar-items/solution.cpp
+++ leetcode/easy/2363-merge-similar-items/solution.cpp
@@ -3,23 +3,24 @@ public:
     vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1,
                                            vector<vector<int>>& items2) {
 
-        unordered_map<int, int> mp;
+        int weight[1001] = {};
 
         for (auto item : items1) {
-            mp[item[0]] += item[1];
+            weight[item[0]] += item[1];
         }
 
         for (auto item : items2) {
-            mp[item[0]] += item[1];
+            weight[item[0]] += item[1];
         }
 
         vector<vector<int>> ans;
 
-        for (auto item : mp) {
-            ans.push_back({item.first, item.second});
-        }
+        for (int value = 1; value <= 1000; value++) {
 
-        sort(ans.begin(), ans.end());
+            if (weight[value] > 0) {
+                ans.push_back({value, weight[value]});
+            }
+        }
 
         return ans;
     }
