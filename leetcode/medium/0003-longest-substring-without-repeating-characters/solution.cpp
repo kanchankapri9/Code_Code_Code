a519a75ca20097c61bdcf2ed35b8f3fd63e0b61a
kanchan kapri <kaprikanchan09@gmail.com>
2026-08-27 23:43:35 +0530

    ✅ Longest Substring Without Repeating Characters | Medium | C++ | Runtime: 304 ms (13.97%) | Memory: 81.5 MB | 2026-08-27

--- leetcode/medium/0003-longest-substring-without-repeating-characters/solution.cpp
+++ leetcode/medium/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        unordered_set<char> st;
+
+        int left = 0;
         int ans = 0;
 
-        for (int i = 0; i < s.size(); i++) {
+        for (int right = 0; right < s.size(); right++) {
 
-            unordered_set<char> st;
-
-            for (int j = i; j < s.size(); j++) {
-
-                if (st.count(s[j])) {
-                    break;
-                }
-
-                st.insert(s[j]);
-
-                ans = max(ans, j - i + 1);
+            while (st.count(s[right])) {
+                st.erase(s[left]);
+                left++;
             }
+
+            st.insert(s[right]);
+
+            ans = max(ans, right - left + 1);
         }
 
         return ans;
